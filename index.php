d4f244c97e8ce7061231299db5841592c26bb7c6
Marcio Yonamine <marcioyonamine@gmail.com>
2015-04-12 11:26:18 -0300

    Estrutura v0.1

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php 
+
+/*
+igSmc v0.1 - 2015
+ccsplab.org - centro cultural são paulo
+*/
+
+// Esta é a página de login do usuário ou de contato com administrador do sistema.
+
+?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
---
+++ perfil/administrador.php
@@ -0,0 +1,4 @@
+<?
+//include para painel administração
+
+ ?>
