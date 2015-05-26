db3f248368743193ad26fcd73806ac549ad8c6b7
Marcio Yonamine <marcioyonamine@gmail.com>
2015-05-26 14:13:23 -0300

    Estrutura inicio

---
+++ perfil/evento.php
@@ -0,0 +1,4 @@
+﻿<h1>Início</h1>
+
+<p>Mensagem inicial</p>
+<p>Perfis aos quais tem acesso</p>
---
+++ perfil/inicio.php
@@ -0,0 +1,17 @@
+﻿<h1>Início</h1>
+
+<p>Mensagem inicial</p>
+<p>Perfis aos quais tem acesso</p>
+
+<h2>Pedidos enviados</h2>
+
+<h2>Pedidos em aberto</h2>
+
+<h2>Área do usuário</h2>
+
+<h2>Relatório de alterações</h2>
+
+<h2>Envio de alterações</h2>
+
+<h2>Anexar arquivos em pedidos enviados</h2>
+
